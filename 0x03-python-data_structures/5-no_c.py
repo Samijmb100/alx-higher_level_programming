208f97f9c499777e17e623506ecdc20d9a64f41b
Samuel <samueldavidm10@gmail.com>
2023-03-05 13:21:22 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord(i): None for i in "Cc"})
+    a = len(my_string)
+    i = 0
+    while i < a:
+        if my_string[i] == c or my_string == C:
+            my_string.translate({ord(i): None})
+        i++
+    return my_string
 
